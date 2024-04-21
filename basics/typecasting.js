546eca1303861f8cc654a404af9baa94b21948b3
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-21 12:43:53 +0000

    renamed folder

---
+++ basics/typecasting.js
@@ -0,0 +1,61 @@
+let str ="yash"
+let num = 1
+let bool = true
+let undif
+let nul = null
+
+/*
+//console.table ([str, num, bool, undif, nul]) returns output in form of a table and refference in index
+console.log (typeof str)
+console.log (typeof num)
+console.log (typeof bool)
+console.log (typeof undif)
+console.log (typeof null)
+*/
+
+/*
+// for converting to string  we use "String(variable)"
+console.log (String(num)) //10 as string
+console.log (typeof String(num))
+
+console.log (String(bool)) // true as string
+console.log (typeof String(bool))
+
+console.log (String(undif)) //undefined as string
+console.log (typeof String(undif))
+
+console.log (String(nul)) // null as string
+console.log (typeof String(nul))
+*/
+
+/*
+// for converting to Number  we use "Number(variable)"
+console.log (Number(str)) //NaN as Number (NaN stands for not a number)
+console.log (typeof Number(str))
+
+console.log (Number(bool)) // 1 as Number
+console.log (typeof Number(bool))
+
+console.log (Number(undif)) //NaN as Number
+console.log (typeof Number(undif))
+
+console.log (Number(nul)) // 0 as Number
+console.log (typeof Number(nul))
+*/
+
+// for converting to Boolean  we use "Boolean(variable)"
+console.log (Boolean(str)) //string with value returns true and empty sting returns false
+console.log (typeof Boolean(str))
+
+console.log (Boolean(num)) // 0 returns fasle and other number reutrns true
+console.log (typeof Boolean(num))
+
+console.log (Boolean(undif)) //false
+console.log (typeof Boolean(undif))
+
+console.log (Boolean(nul)) // false
+console.log (typeof Boolean(nul))
+
+console.log ( 1 + 2 + 3 )   // output 6
+console.log ( '1' + 2 + 3 ) // output 123
+console.log ( 1 + 2 + '3' ) // output 33
