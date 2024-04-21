a75807fd77c4d4b81e1b8aa0725fcce56bb87b1a
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-21 13:00:24 +0000

    cmparision operator working

---
+++ comparision.js
@@ -0,0 +1,23 @@
+console.log ( 2 == 0) // false
+console.log ( 2 > 0)  //true
+console.log ( 2 < 0)  // false
+console.log ( 2 <= 0) // false
+console.log ( 2 >= 0) //true
+
+/*
+
+== and === are known as equality check operators 
+
+< , > , <= , >= are know as comparision operators
+
+both equality and comparision operators works differently in turms of comparision and data type conversion
+
+example
+*/
+
+console.log ( '2' == 0)  // false
+console.log ( '2' > 0)   // true
+
+console.log ( null == 0) // false
+console.log ( null < 0)  // false
+console.log ( null >= 0) // true
