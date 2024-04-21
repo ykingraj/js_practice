ac1244099370814506ecbbb2328e2923e68105e7
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-21 14:42:11 +0000

    update

--- basics/symbol.js
+++ basics/symbol.js
@@ -4,3 +4,6 @@ let a = Symbol(2)
 let b = Symbol(2)
 
 console.log (a==b)
+console.log(a)
+
+//we can see that a and b both have 2 value as symbol but the comparision of the values return false because each symbol return different value irespective of there assigned value
