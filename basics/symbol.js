b7916fa321b1cba939dbd1b00335002008c17c7b
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-21 14:57:44 +0000

    update

--- basics/symbol.js
+++ basics/symbol.js
@@ -7,3 +7,19 @@ console.log (a==b)
 console.log(a)
 
 //we can see that a and b both have 2 value as symbol but the comparision of the values return false because each symbol return different value irespective of there assigned value
+
+/*
+1) Primitive Datatypes (values returned from typeof function)
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes (values returned from typeof function)
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
