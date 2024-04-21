7bd428cd2c81020f265650cda5895a55b9b4c394
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-21 13:03:36 +0000

    update

--- basics/comparision.js
+++ basics/comparision.js
@@ -21,3 +21,7 @@ console.log ( '2' > 0)   // true
 console.log ( null == 0) // false
 console.log ( null < 0)  // false
 console.log ( null >= 0) // true
+
+/* 
+here equlity operator changes null to NaN and then compare where as comparision changes it to 0 and then compare. Thus we are getting true in line 3
+*/
