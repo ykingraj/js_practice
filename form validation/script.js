3f4521ff235c2b3b6048e3343457b6d122172869
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-24 04:50:00 +0000

    update

--- form validation/script.js
+++ form validation/script.js
@@ -1,70 +1,65 @@
-function validateForm() {
-    const name =
-        document.getElementById("name").value
-    const address =
-        document.getElementById("address").value
-    const email =
-        document.getElementById("email").value
-    const password =
-        document.getElementById("password").value
-    const subject =
-        document.getElementById("subject").value
-    const agree =
-        document.getElementById("agree").checked
 
-    const nameError =
-        document.getElementById("name-error")
-    const addressError = document.getElementById(
-        "address-error"
-    )
-    const emailError = document.getElementById(
-        "email-error"
-    )
-    const passwordError = document.getElementById(
-        "password-error"
-    )
-    const subjectError = document.getElementById(
-        "subject-error"
-    )
-    const agreeError = document.getElementById(
-        "agree-error"
-    )
+function validateForm() {
+    const name = document.getElementById("name").value
+
+    const address = document.getElementById("address").value
+
+    const email = document.getElementById("email").value
+
+    const password = document.getElementById("password").value
+
+    const subject = document.getElementById("subject").value
+
+    const agree = document.getElementById("agree").checked
+
+    const nameError =document.getElementById("name-error")
+
+    const addressError = document.getElementById("address-error")
+
+    const emailError = document.getElementById("email-error")
+
+    const passwordError = document.getElementById("password-error")
+
+    const subjectError = document.getElementById("subject-error")
+
+    const agreeError = document.getElementById("agree-error")
+
+    let isValid=true
 
     if (name === "") {
-        nameError.innerText =
-            "Please enter your name properly."
+        nameError.innerText = "Please enter your name properly."
         isValid = false
     }
 
     if (address === "") {
-        addressError.innerHTML =
-            "Please enter your address."
+        addressError.innerHTML ="Please enter your address."
         isValid = false
     }
 
     if (email === "" || !email.includes("@")) {
-        emailError.textContent =
-            "Please enter a valid email address."
+        emailError.textContent = "Please enter a valid email address."
         isValid = false
     }
 
     if (password === "" || password.length < 6) {
-        passwordError.textContent =
-            "Please enter a password with at least 6 characters."
+        passwordError.textContent = "Please enter a password with at least 6 characters."
         isValid = false
     }
 
     if (subject === "") {
-        subjectError.textContent =
-            "Please select your course."
+        subjectError.textContent = "Please select your course."
         isValid = false
     }
 
     if (!agree) {
-        agreeError.textContent =
-            "Please agree to the above information."
+        agreeError.textContent = "Please agree to the above information."
         isValid = false
     }
+
+    if (isValid==true) {
+        alert ("Form filled successfully")
+    }
+
 
     return isValid
 }
