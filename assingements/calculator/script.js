7b85a818769d4d4f42746ec99deb4f07836317c4
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-29 08:32:26 +0000

    ggs

---
+++ assingements/calculator/script.js
@@ -0,0 +1,37 @@
+let x= ""
+let val = ""
+let result = "wroking on it"
+
+function valadd(a)
+{
+    val = val + a
+    document.getElementById("result").innerText= val
+}
+
+function show()
+{
+    document.getElementById("result").innerText= result
+}
+
+function fundiv() {
+
+}
+
+function fundmulti() {
+    
+}
+
+function funadd() {
+    
+}
+
+function funsub() {
+    
+}
+
+function clear()
+{
+    val = "0"
+    document.getElementById("result").innerText= val
+
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+let x  = "123abc"
+let len= x.length
+
+for ( let i = 0; i<len; i++){
+    console.log ((typeof x[i]))
+    console.log ( x [i])
+}
