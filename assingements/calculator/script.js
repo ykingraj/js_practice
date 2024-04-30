e44292e4f125a6cf1aff38f466fa42348b06129f
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-04-30 06:30:47 +0000

    t

--- assingements/calculator/script.js
+++ assingements/calculator/script.js
@@ -2,57 +2,99 @@ let x = ""
 let val = ""
 let result = "wroking on it"
 let decimalpointer = -1
-let statement = []
-let operation = []
+let stm = []
+let stmlen = stm.length
+let opt = []
+let optlen = opt.length
 
-function valadd(a)
-{
+function valadd(a) {
     val = val + a
     document.getElementById("result").innerText= val
     console.log ("553")
 }
 
-function valadd2()
-{
+function valadd2() {
     decimalpointer = val.indexOf(".")
-    if (decimalpointer >=0 ){
-    }
+    if (decimalpointer >=0 ) {
+    }  // nothing will happen
     else{
         val = val + "."
     }
     document.getElementById("result").innerText= val
 }
 
-function show()
-{
-    document.getElementById("result").innerText= result
-}
-
-function fundiv() 
-{
-    statement.push(val)
-    operation.push("/")
+function fundiv() {
+    stm.push(val)
+    opt.push("/")
     val= ""
     document.getElementById("result").innerText= val
 }
 
-function fundmulti() 
-{
-    
+function fundmulti() {
+    stm.push(val)
+    opt.push("*")
+    val= ""
+    document.getElementById("result").innerText= val
 }
 
-function funadd() 
-{
-    
+function funadd() {
+    stm.push(val)
+    opt.push("+")
+    val= ""
+    document.getElementById("result").innerText= val
 }
 
-function funsub() 
-{
-    
+function funsub() {
+    stm.push(val)
+    opt.push("-")
+    val= ""
+    document.getElementById("result").innerText= val
 }
 
-function cl()
-{
-    val = 0
+function cl() {
+    val = "0"
     document.getElementById("result").innerText= 0;
 }
+
+function show() {
+    document.getElementById("result").innerText= val
+    //alert(stm)
+    //alert(opt)
+
+    
+}
+
+
+
+let ss 
+let oo
+val = 0
+
+
+for (let s = 1; s <= (stmlen-1); s++){
+    ss = Number(stm[s])
+    
+    for (let o = 0; o <= (optlen-1); o++){
+        oo = opt[o]
+
+        if (oo == "+"){
+
+            val += ss
+        }
+
+        if (oo == "-"){
+
+            val -= ss
+        }
+
+        if (oo == "/"){
+
+            val /= ss
+        }
+
+        if (oo == "*"){
+
+            val *= ss
+        } 
+    }
+}   
--- test.js
+++ test.js
@@ -1,3 +1,38 @@
-let x = "shdh.sfg"
-let d = x.indexOf(".")
-console.log (d)
+let stm = [1,2,3,4]
+let stmlen = stm.length
+let opt = ["+","-","+"]
+let optlen = opt.length
+let ss 
+let oo
+val = 0
+
+
+for (let s = 0; s <= (stmlen-1); s++){
+    ss = Number(stm[s])
+    
+    for (let o = 0; o <= (optlen-1); o++){
+        oo = opt[o]
+    }
+
+    if (oo == "+"){
+            val += ss
+            console.log(val)
+        }
+
+    if (oo == "-"){
+            val -= ss
+            console.log(val)
+        }
+
+    if (oo == "/"){
+            val /= ss
+            console.log(val)
+        }
+
+    if (oo == "*"){
+            val *= ss
+            console.log(val)
+        } 
+    
+}   
+//console.log (val)
