46a47cb672663d1f75dd34bf4e59c98c121f6a2e
ykingraj <136958320+ykingraj@users.noreply.github.com>
2024-05-02 11:42:07 +0000

    part 2

---
+++ basic/string functions.js
@@ -0,0 +1,66 @@
+/*
+//lenght() function
+Returns the length of a string. The length property of a string is a read-only property that returns the number of characters in the string.
+
+syntax : string.length()
+
+//charAt() function
+Returns the character at the specified index. If the index is out of range, charAt() returns an empty string.
+
+syntax : string.charAt(index)
+
+//charCodeAt() function
+Returns the Unicode value of the character at the specified index.
+
+syntax : string.charCodeAt(index)
+
+//substring() function
+Returns a new string containing characters from startIndex to endIndex - 1.
+
+syntax : substring(startIndex, endIndex)
+
+//slice() function
+Returns a section of a string and allows the extraction of a part of the string.
+
+syntax : string.slice(startIndex, endIndex)
+
+//indexOf() function
+Returns the index of the first occurrence of searchValue, starting from startIndex.
+
+syntax : string.indexOf(searchValue, startIndex)
+
+//lastIndexOf() function
+Returns the index of the last occurrence of searchValue, searching backwards.
+
+syntax : string.lastIndexOf(searchValue, startIndex)
+
+//toUpperCase() function
+Converts the string to uppercase.
+
+syntax : string.toUpperCsae()
+
+//toLowerCase() function
+Converts the string to lowercase.
+
+syntax : string.toUpperCsae()
+
+//replace() function
+Replaces searchValue with replaceValue.
+
+syntax : string.replace(searchValue, replaceValue)
+
+//split() function 
+Splits the string into an array of substrings based on the separator.
+
+syntax : string.split(separator, limit)
+
+//join() function
+Joins the elements of an array into a string, using the separator.
+
+syntax : string.join(separator)
+
+//trim() function
+Removes leading and trailing whitespaces from a string.
+
+syntax : str.trim()
+*/
